c4e2c2f92e697d539f230fba38864009a3fde45c
sillypears <sillypairs@gmail.com>
2022-03-26 22:33:23 -0400

    Did I break it ?

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import urlparse
 import os
@@ -43,7 +43,13 @@ def create_app():
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
-        
+    @app.before_request
+    def before_request():
+        if 'DYNO' in os.environ:
+            if request.url.startswith('http://'):
+                url = request.url.replace('http://', 'https://', 1)
+                code = 301
+                return redirect(url, code=code)   
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
